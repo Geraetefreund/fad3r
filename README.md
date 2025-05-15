7cf304b1c6288e259a5283240966d96c74edfe10
Geraetefreund <friteuse@arcor.de>
2025-05-15 13:28:11 +0100

    v0.3 add curves, changed eeprom layout

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # fad3r
 
+v0.3 / 2025-05-15
+
+Add: support for curves. So far lin, log, exp.
+change: eeprom setup.
+
+for example: ```F0 7D 43 <preset> <cc1> <cc2> <cc3> <ch1> <ch2> <ch3> <cv1> <cv2> <cv3> F7```
+
+| Index | Byte      | Description            |
+| ----- | --------- | ---------------------- |
+| 0     | `0xF0`    | SysEx start            |
+| 1     | `0x7D`    | Non-commercial ID      |
+| 2     | `0x43`    | Dump command           |
+| 3     | `Preset`  | Preset index           |
+| 4–6   | `CC1–CC3` | Control change numbers |
+| 7–9   | `CH1–CH3` | MIDI channels          |
+| 10–12 | `CV1–CV3` | Curve values           |
+| 13    | `0xF7`    | SysEx end              |
+
+curves: 
+0 linear
+1 logarithmic
+2 exponential
+
+added curve per fader, so changed the eeprom layout a bit.
+
+=================
 v0.2 / 2025-05-14
 
 Improved preset storage in EEPROM. Faders can now have indiviual MIDI channels.  
--- main.ino
+++ main.ino
@@ -1,5 +1,5 @@
 //Fad3r
-//v0.2 / 2025-05-14
+//v0.3 / 2025-05-15
 
 #include <EEPROM.h>
 
@@ -7,16 +7,15 @@
 Preset      Addr      Contents
 0           0         CC1 CC2 CC3
             3         CH1 CH2 CH3
-1           6         ...
+            6         Curve1, Curve2, Curve3
+1           9         ...
 ...         ...       ...
-Active      24        Current preset
-*/
+Active      36        Current preset */
+
 const int NUM_FADERS = 3;
 const int NUM_PRESETS = 4;
-const int BYTES_PER_PRESET = 6; // 3 CC + 3 CH
+const int BYTES_PER_PRESET = 9; // 3 CC + 3 CH + 3 curve
 const int ACTIVE_PRESET_ADDR = NUM_PRESETS * BYTES_PER_PRESET; // stores location pointing to current preset
-
-const int EEPROM_address[3] = {0, 1, 2};
 
 // Fader analog input pins
 const int pins[NUM_FADERS] = {A0, A1, A2}; // A0 = Fader 1, A1 = Fader 2, A2 = Fader 3
@@ -24,8 +23,10 @@ const int pins[NUM_FADERS] = {A0, A1, A2}; // A0 = Fader 1, A1 = Fader 2, A2 = F
 // MIDI CC and MIDI channel mappings
 uint8_t cc[NUM_FADERS];
 uint8_t channel[NUM_FADERS];
+uint8_t curve[NUM_FADERS]; // 0 = linear, 1 = logarithmic, 2 - exponential
 
 uint8_t currentPreset = 0;
+
 bool sysexHandled = false;
 
 // Previous analog values for change detection and smoothing
@@ -64,12 +65,35 @@ void loop() {
   }
 }
 
+int applyCurve(int raw, uint8_t type) {
+  float norm = raw / 1023.0;
+  float curved = 0.0;
+
+  switch (type) {
+    case 0x00: // linear
+    curved = norm;
+    break;
+
+    case 0x01: // logarithmic
+    curved = log10(9 * norm +1); //log(1) = 0, log(10) = 1
+    break;
+
+    case 0x02: // exponential
+    curved = (exp(norm) -1) / (exp(1) -1);
+    break;
+  }
+
+  int result = int(curved * 127);
+  return constrain(result, 0, 127);
+}
+
 // Reads fader value and checks for significant change
 int getFaderValue(int index) {
   int value = analogRead(pins[index]); // 0–1023
   if (abs(value - oldValue[index]) >= 8) { // Change threshold
     oldValue[index] = value;
-    return (value < 8) ? 0 : (value >> 3); // Scale to 0–127
+    //return (value < 8) ? 0 : (value >> 3); // Scale to 0–127
+    return applyCurve(value, curve[index]);
   }
   return 255; // No change
 }
@@ -80,6 +104,7 @@ void loadPreset(uint8_t preset) {
   for (int i = 0; i < NUM_FADERS; i++) {
     cc[i] = EEPROM.read(base + i);
     channel[i] = EEPROM.read(base + NUM_FADERS + i);
+    curve[i] = EEPROM.read(base + 2 * NUM_FADERS +i);
   }
 }
 
@@ -96,12 +121,13 @@ void savePreset(uint8_t preset) {
   for (int i = 0; i < NUM_FADERS; i++) {
     EEPROM.update(base + i, cc[i]);
     EEPROM.update(base + NUM_FADERS + i, channel[i]);
+    EEPROM.update(base + 2 * NUM_FADERS + i, curve[i]);
   }
 }
 
 void dumpPreset(uint8_t preset) {
   if (preset >= NUM_PRESETS) return;
-  uint8_t data[11]; // oops was 10 before.
+  uint8_t data[14]; // SysEx: F0 7D 43 <Preset> CC1 CC2 CC3 CH1 CH2 CH3 CV1 CV2 CV3 F7
   data[0] = 0xF0;
   data[1] = 0x7D;
   data[2] = 0x43; // Dump command
@@ -109,10 +135,11 @@ void dumpPreset(uint8_t preset) {
   int base = preset * BYTES_PER_PRESET;
   for (int i = 0; i < NUM_FADERS; i++) {
     data[4 + i] = EEPROM.read(base + i);  // CC
-    data[4 + NUM_FADERS + i] = EEPROM.read(base + NUM_FADERS + i); // CH
+    data[7 + i] = EEPROM.read(base + NUM_FADERS + i); // CH
+    data[10 + i] = EEPROM.read(base + 2 * NUM_FADERS +i);
   }
-  data[10] = 0xF7;
-  usbMIDI.sendSysEx(11, data, true);
+  data[13] = 0xF7;
+  usbMIDI.sendSysEx(14, data, true);
 }
 
 void handleSysEx() {
@@ -137,11 +164,12 @@ void handleSysEx() {
         case 0x40: applyPreset(preset); break;
         case 0x41: savePreset(preset); break;
         case 0x42:
-          if (len == 11 && preset < NUM_PRESETS) {
+          if (len == 14 && preset < NUM_PRESETS) {
             int base = preset * BYTES_PER_PRESET;
             for (int i = 0; i < NUM_FADERS; i++) {
               EEPROM.update(base + i, data[4 + i]);
-              EEPROM.update(base + NUM_FADERS + i, data[4 + NUM_FADERS + i]);
+              EEPROM.update(base + NUM_FADERS + i, data[7 + i]);
+              EEPROM.update(base + 2 * NUM_FADERS + i, data[10 +i]);
             }
             applyPreset(preset);
           }
